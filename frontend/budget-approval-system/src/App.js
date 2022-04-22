b0f9d3a00d33f4a56e34a83fefbc21d5b5ccfb02
amishaaggarwal <amishaaggarwal10@gmail.com>
2022-04-22 13:28:55 +0530

    theme files separated

--- frontend/budget-approval-system/src/App.js
+++ frontend/budget-approval-system/src/App.js
@@ -1,8 +1,14 @@
-import Routing from 'util/Routing/Routing';
-import './App.css';
+import Routing from "util/Routing/Routing";
+import { ThemeProvider } from "@mui/material/styles";
+import AppTheme from "Constants/AppTheme";
+import "./App.css";
 
 function App() {
-  return <Routing/>
+  return (
+    <ThemeProvider theme={AppTheme}>
+      <Routing />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- frontend/budget-approval-system/src/pages/Login/Login.js
+++ frontend/budget-approval-system/src/pages/Login/Login.js
@@ -1,17 +1,9 @@
 import GoogleIcon from "@mui/icons-material/Google";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Checkbox from "@mui/material/Checkbox";
-import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
+import {
+  Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid,
+  Link, TextField, Typography
+} from "@mui/material";
 import {
   GoogleAuthProvider,
   signInWithEmailAndPassword,
@@ -40,12 +32,6 @@ function Copyright(props) {
     </Typography>
   );
 }
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
 
 export default function SignIn() {
   const navigate = useNavigate();
@@ -104,64 +90,62 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box sx={styles.outerBox}>
-          <Avatar sx={styles.loginIcon}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate mt={1}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button type="submit" fullWidth variant="contained" mt={3} mb={2}>
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-          <Typography>OR</Typography>
-          <Button
-            variant="contained"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box sx={styles.outerBox}>
+        <Avatar sx={styles.loginIcon}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate mt={1}>
+          <TextField
+            margin="normal"
+            required
             fullWidth
-            onClick={() => signInWithGoggle()}
-          >
-            <GoogleIcon sx={{ mr: "10px" }} />
-            Continue with Google
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" mt={3} mb={2}>
+            Sign In
           </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright mt={8} mb={4} />
-      </Container>
-    </ThemeProvider>
+        <Typography>OR</Typography>
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={() => signInWithGoggle()}
+        >
+          <GoogleIcon sx={{ mr: "10px" }} />
+          Continue with Google
+        </Button>
+      </Box>
+      <Copyright mt={8} mb={4} />
+    </Container>
   );
 }
