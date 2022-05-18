ee372aac7e6ac2180a7651967af4168a57139eea
Amisha Aggarwal <amishaaggarwal10@gmail.com>
2022-05-18 17:38:48 +0530

    Loader added

--- frontend/budget-approval-system/src/util/Routing/Routing.js
+++ frontend/budget-approval-system/src/util/Routing/Routing.js
@@ -1,13 +1,15 @@
-// import Notfound from "pages/NotFound/NotFound";
+import Loader from "components/Loader/Loader";
 import Dashboard from "pages/Dashboard/Dashboard";
 import Login from "pages/Login/Login";
-import Error404 from "pages/NotFound/Error404";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 
+const Error404 = lazy(() => import("pages/NotFound/Error404"));
+
 function Routing() {
   return (
-    <div>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route element={<PrivateRoute />}>
@@ -15,7 +17,7 @@ function Routing() {
         </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
-    </div>
+    </Suspense>
   );
 }
 
