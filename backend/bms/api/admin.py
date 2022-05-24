2a4bd576fc783fc4679746c8b79685be7e8210a6
Anagh Kumar <anagh.kumar@aurigait.com>
2022-05-24 06:25:55 +0530

    Backend Complete work till notification

--- backend/bms/api/admin.py
+++ backend/bms/api/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Bill
+from .models import CustomUser, Bill, Notification
 # Register your models here.
 
 admin.site.register(CustomUser)
 
+
 @admin.register(Bill)
 class BillAdmin(admin.ModelAdmin):
     list_display = ('project_name', 'date_of_issue', 'bill_status',
-                        'comments', 'approved_by', 'approved_on', 'issued_by')
+                    'comments', 'approved_by', 'approved_on', 'issued_by')
 
 
+admin.site.register(Notification)
